ad83313445ec9220c9e34331eea5f1204aab36d4
niveditham348 <nm5062020@gmail.com>
2024-09-17 16:02:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+	display.value = '';
+}
+
+function deleteLast() {
+	display.value = display.value.slice(0, -1);
+}
+
+function append(char) {
+	display.value += char;
+}
+
+function calculate() {
+	try {
+		display.value = eval(display.value);
+	} catch (e) {
+		display.value = 'Error';
+	}
+	
+
+	
+}
